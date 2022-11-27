d287fc409303860de1d5a2b865c9aadc2e1527c0
Sugar Edge <117456168+rversecollective@users.noreply.github.com>
2022-11-27 22:24:41 +0100

    d

--- new/js/script.js
+++ new/js/script.js
@@ -1,3 +1,34 @@
+Array.prototype.random = function () {
+	return this[Math.floor((Math.random() * this.length))];
+}
+
+function getRandomSymbol() {
+	var symbol = symbols.random();
+	return (symbol == $('#symbol').html()) ? getRandomSymbol() : symbol;
+}
+
+async function fadeInSymbol() {
+	$('#symbol').animate({ opacity: 1 }, 1000, function () {
+		setTimeout(function () {
+			fadeOutSymbol();
+		}, 1000);
+	});
+}
+
+async function fadeOutSymbol() {
+	$('#symbol').animate({ opacity: 0 }, 1000, function () {
+		$('#symbol').html(getRandomSymbol());
+		fadeInSymbol();
+	});
+}
+
+var speed = 900;
+var symbols = ['/', '_', '&', '!', '=', ';', '+', '>', '-', '#', ':', '×', '÷'];
+
+setTimeout(function () {
+	fadeOutSymbol();
+}, 500);
+
 $('#above-button').on('click', function() {
 	$('html, body').animate({ scrollTop: 0 } );
 });
